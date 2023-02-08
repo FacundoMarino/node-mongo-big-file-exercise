a57fe919187957913d1a35f38a14430d0456915f
Facundo Marino <38021147+FacundoMarino@users.noreply.github.com>
2023-02-08 14:52:15 -0300

    ADD: Se agrega funcionalidad de manejo de archivos
    Se agrega funcionalidad de manejo de archivos filtrados por csv para carga en base de datos.

--- src/controller.js
+++ src/controller.js
@@ -1,27 +1,42 @@
-const Records = require('./records.model');
+const Records = require("./records.model");
+const csvtojson = require("csvtojson");
 
 const upload = async (req, res) => {
-    const {file} = req;
+  const { file } = req;
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
-
-    return res.status(200).json({ message: 'some response' });
+  try {
+    //Convierto archivo csv en json con librería csvtojson
+    await csvtojson()
+      .fromFile(file.path) // ubicación del archivo
+      .then((csvData) => {
+        Records.insertMany(csvData).then(() => {
+          //uso la data del json para enviarla en el schema a mongodb
+          res.json({
+            ok: true,
+            msg: "El archivo se ha subido correctamente",
+          });
+        });
+      });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      ok: false,
+      msg: "Error al subir el archivo",
+    });
+  }
 };
 
 const list = async (_, res) => {
-    try {
-        const data = await Records
-            .find({})
-            .limit(10)
-            .lean();
-        
-        return res.status(200).json(data);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+  try {
+    const data = await Records.find({}).limit(10).lean();
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 module.exports = {
-    upload,
-    list,
+  upload,
+  list,
 };
--- src/route.js
+++ src/route.js
@@ -1,11 +1,37 @@
-const express = require('express');
-const multer = require('multer');
-const { upload, list } = require('./controller');
+const express = require("express");
+const multer = require("multer");
+const { upload, list } = require("./controller");
 
 const router = express.Router();
-const uploadFile = multer({ dest: './_temp' });
 
-router.post('/upload', uploadFile.single('file'), upload);
-router.get('/records', list);
+const uploadFile = multer({
+  //Defino lugar de almacenamiento
+  storage: multer.diskStorage({
+    destination: "./_temp",
+
+    //Defino nombre de archivo para que no se suban dos archivos con el mismo nombre
+
+    filename: (req, file, cb) => {
+      cb("", `${Date.now()}-${file.originalname}`);
+    },
+  }),
+
+  //Hago filtro de archivos para que solo se puedan subir tipos de archivo, si se desean más se pueden sumar a un array ej ["text/txt", "text/csv", "image/png"]
+
+  fileFilter: (req, file, cb) => {
+    file.mimetype === "text/csv"
+      ? cb(null, true)
+      : cb(new Error("Error de tipo de archivo"), false);
+  },
+
+  //Limito tamaño del archivo para que solo se puedan subir hasta una cantidad maxima.
+
+  limits: {
+    fileSize: 80000000,
+  },
+});
+
+router.post("/upload", uploadFile.single("file"), upload);
+router.get("/records", list);
 
 module.exports = router;
